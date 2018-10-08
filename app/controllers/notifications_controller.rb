c1f713eaf1c66d6e982569e4fc0c5b68c5669339
Jukinner <jukinner@gmail.com>
2018-10-07 22:32:33 -0600

    Added test for additional api validations

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,14 +1,34 @@
 class NotificationsController < ApplicationController
-  respond_to :json, :html
+  respond_to :json
+
+  def index
+  end
 
   def create
-    @notification = Notification.create(params.permit(:phone, :body, :source_app))
+    @notification = Notification.create(notification_params)
 
     respond_to do |format|
-      format.json { render json: @notificaton.to_json, action: 'show', status: :created, location: @notification }   
+      if @notification.save
+        format.json { render json: @notificaton, action: 'show', status: :created } 
+      else
+        format.json { render json: @notificaton.errors, status: :unprocessable_entity } 
+      end
+        
     end
   end
 
   def show
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def notification_params
+      params.require(:notification).permit(:phone, :body, :source_app)
+    end
 end
--- spec/requests/notification_spec.rb
+++ spec/requests/notification_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe Notification, type: :request  do
     expect(response.content_type).to eq("application/json")
     expect(response).to have_http_status(:created)
   end
+
+  it "creates a Notification" do 
+    headers = { "CONTENT_TYPE" => "application/json" }
+    post "/notifications",
+    :params => {
+      notification: {
+        phone: "1234567890",
+        body: "My test Message",
+      }
+    }, :headers => headers
+
+    expect(response.content_type).to eq("application/json")
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
 end
