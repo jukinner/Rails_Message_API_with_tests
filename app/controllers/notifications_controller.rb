866101f84a49018033faae93413666fb64c91947
Jukinner <jukinner@gmail.com>
2018-10-07 18:04:30 -0600

    stop point for failing rspec tests regarding finding create action:

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,14 @@
 class NotificationsController < ApplicationController
+  respond_to :json, :html
+
+  def create
+    @notification = Notification.create(params.permit(:phone, :body, :source_app))
+
+    respond_to do |format|
+      format.json { render json: @notificaton.to_json, action: 'show', status: :created, location: @notification }   
+    end
+  end
+
+  def show
+  end
 end
---
+++ spec/requests/notification_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Notification, type: :request  do 
+
+  it "creates a Notification" do 
+    headers = { "CONTENT_TYPE" => "application/json" }
+    post "/notifications",
+    :params => {
+      notification: {
+        phone: "1234567890",
+        body: "My test Message",
+        source_app: "My_test_app_name"
+      }
+    }, :headers => headers
+
+    expect(response.content_type).to eq("application/json")
+    expect(response).to have_http_status(:created)
+  end
+end
