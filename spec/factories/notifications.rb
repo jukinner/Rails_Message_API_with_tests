1348dac7abcac78d85c164d16c87e2fc50d5bcfd
Justin Kinner <jkinner@gmail.com>
2018-10-08 10:13:08 -0600

    Updated notifications controller, figured out controller action issue and updated tests

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :notification do
-    phone "5555555555"
-    body "My message"
-    source_app "some_app"
+    phone { "5555555555" }
+    body { "My message" }
+    source_app { "some_app" }
   end
 
   factory :second_notification, class: 'Notification' do
-    phone "8568568568"
-    body "My 2nd message"
-    source_app "some_app x2"
+    phone { "8568568568" }
+    body { "My 2nd message" }
+    source_app { "some_app x2" }
   end
 end
--- spec/requests/notification_spec.rb
+++ spec/requests/notification_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Notification, type: :request  do 
 
+  it 'returns ok and a 200' do
+    get notifications_path
+    expect(response.status).to eq(200)
+    expect(response.body).to eq('ok')
+  end
+
   it "creates a Notification" do 
-    headers = { "CONTENT_TYPE" => "application/json" }
+
     post "/notifications",
     :params => {
       notification: {
@@ -11,7 +17,8 @@ RSpec.describe Notification, type: :request  do
         body: "My test Message",
         source_app: "My_test_app_name"
       }
-    }, :headers => headers
+    }, xhr: true
+
 
     expect(response.content_type).to eq("application/json")
     expect(response).to have_http_status(:created)
@@ -25,7 +32,7 @@ RSpec.describe Notification, type: :request  do
         phone: "1234567890",
         body: "My test Message",
       }
-    }, :headers => headers
+    }, xhr: true
 
     expect(response.content_type).to eq("application/json")
     expect(response).to have_http_status(:unprocessable_entity)
