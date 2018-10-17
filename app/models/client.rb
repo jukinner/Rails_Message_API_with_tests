7d8f5718aa1d7318a7ba594218f81da26314ff15
Justin Kinner <jkinner@gmail.com>
2018-10-16 23:22:21 -0600

    added api key generator and tests to back it up

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,12 @@ class Client < ApplicationRecord
   validates_presence_of :source_app, :api_key
   validates_uniqueness_of :source_app, :api_key
   has_many :notifications, foreign_key: 'source_app', primary_key: 'source_app'
+
+  before_validation :set_api_key
+
+  private
+
+  	def set_api_key
+  		self.api_key = ApiKey.generator
+  	end
 end
